94d1dd2d01c46d170905728cba0c64440bbe66e6
shahjavadeveloper <shahjavadeveloper@gmail.com>
2023-07-06 16:26:47 +0530

    first commit

---
+++ src/main/java/com/gaia/igcaqr/Igcaqr1Application.java
@@ -0,0 +1,20 @@
+package com.gaia.igcaqr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Igcaqr1Application {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(Igcaqr1Application.class);
+
+	public static void main(String[] args) {
+		System.out.println("sdkfskjfhkjs");
+		
+		SpringApplication.run(Igcaqr1Application.class, args);
+	}
+
+}
